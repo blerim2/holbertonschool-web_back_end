f7d2ead4eb74a72a79c60961ddb7da662ad635f5
blerim2 <blerimballiu19@gmail.com>
2024-05-02 16:37:24 +0000

    update

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Python script that provides some stats about Nginx logs stored in, MongoDB"""
+"""Python script that provides some stats about Nginx logs stored in MongoDB"""
 
 from pymongo import MongoClient
 
