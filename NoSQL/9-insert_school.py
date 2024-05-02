ca141a76c0b18a609ab22c315515b2c3e24034f0
blerim2 <blerimballiu19@gmail.com>
2024-05-02 12:37:49 +0200

    update

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+NoSQL
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''
+    function that inserts a new document in a collection based on kwargs
+    '''
+    return (mongo_collection.insert_one(kwargs)).inserted_id
