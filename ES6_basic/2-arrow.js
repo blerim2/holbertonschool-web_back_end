219df4d4186886f5b7ec3cdbc3f99f1bf12e193c
blerim2 <blerimballiu19@gmail.com>
2024-05-07 17:30:57 +0200

    arrow function

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+  
+    const self = this;
+    this.addNeighborhood = (newNeighborhood) => {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
+    };
+  }
