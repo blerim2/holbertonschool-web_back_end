d7e5b26ffdad951c2c35888c868f0653cd00eaa8
blerim2 <blerimballiu19@gmail.com>
2024-05-08 10:23:53 +0200

    interpolation

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
