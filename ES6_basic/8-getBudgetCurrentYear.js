5f8677069b12567df24e144577760d82570ca363
blerim2 <blerimballiu19@gmail.com>
2024-05-08 10:35:45 +0200

    budget

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const year = getCurrentYear();
+    const budget = {
+      [`income-${year}`]: income,
+      [`gdp-${year}`]: gdp,
+      [`capita-${year}`]: capita,
+    };
+  
+    return budget;
+  }
