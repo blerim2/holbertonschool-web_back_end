b52145ff6353015d09ad5fbc8ec115d136de1f03
blerim2 <blerimballiu19@gmail.com>
2024-05-10 10:48:29 +0200

    new skyhigh class

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/*eslint-disable*/
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
