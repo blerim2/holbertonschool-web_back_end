47c43bbf9c2c58444c5a860d903124f3bc8015c0
blerim2 <blerimballiu19@gmail.com>
2024-05-17 09:44:43 +0200

    get response

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+const guardrail = (mathFunction) => {
+    const queue = [];
+    try {
+      queue.push(mathFunction());
+    } catch (error) {
+      queue.push(error.toString());
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  };
+  
+  export default guardrail;
