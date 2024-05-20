9994e8e75b3266f02a9fda748f08a794fdc93ec4
blerim2 <blerimballiu19@gmail.com>
2024-05-20 21:21:03 +0200

    update

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+/*eslint-disable*/
+export default function getStudentsByLocation(listStudents, city) {
+    if (!Array.isArray(listStudents)) {
+      return [];
+    }
+    return listStudents.filter((student) => (student.location === city));
+  }
---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,4 @@
+/*eslint-disable*/
+const getStudentIdsSum = (list) => list.reduce((total, student) => total + student.id, 0);
+
+export default getStudentIdsSum;
