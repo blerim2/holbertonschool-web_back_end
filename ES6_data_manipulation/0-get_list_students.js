12959cfc61b5ad58435f62e8fbcceb9ec72706a7
blerim2 <blerimballiu19@gmail.com>
2024-05-20 11:39:49 +0200

    first commit

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getlistStudents () {
+    return [
+        { id: 1, firstName: 'Gulialaume', location: 'San Francisco'},
+        { id: 2, firstName: 'James', location: 'Columbia'},
+        { id: 5, firstName: 'Serena', location: 'San Francisco'}
+    ];
+}
