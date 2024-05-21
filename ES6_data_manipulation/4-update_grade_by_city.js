31f31912603168deaa1a65ab8c84eabc917d2f1d
blerim2 <blerimballiu19@gmail.com>
2024-05-21 11:44:10 +0200

    updategrade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+const updateStudentGradeByCity = (students, city, newGrade) => students
+  .filter((student) => student.location === city)
+  .map((item) => {
+    const newRecord = { ...item };
+
+    const newStudent = newGrade.find((student) => student.studentId === item.id);
+    if (newStudent) newRecord.grade = newStudent.grade;
+    else newRecord.grade = 'N/A';
+    return newRecord;
+  });
+
+export default updateStudentGradeByCity;
